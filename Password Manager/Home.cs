0499406e4bda0c56e1559201d9e2da28688d711f
abdo42wa <abdelrahman-ahmed-ali-mohamed.elsayed@stud.viko.lt>
2021-05-18 23:12:52 +0300

    fixing bugs

--- Password Manager/Home.cs
+++ Password Manager/Home.cs
@@ -50,8 +50,8 @@ namespace Password_Manager
             try
             {
                 string path = @"C:\\Users\\abdoa\\Desktop\\VIKO 4 Semestar\\Information security\\Password Manager\\Password Manager\\bin\\Debug\\Users\\" + HomeUsername.Text + ".txt";
-            
-                FileEncrypt(path, keyy);
+                ASE sE = new ASE();
+                sE.FileEncrypt(path, keyy);
                
                 MessageBox.Show("file is encrypted" + path);
             }
@@ -63,87 +63,6 @@ namespace Password_Manager
             this.Close();
 
         }
-      /*  private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
-        {
-            string path = @"C:\\Users\\abdoa\\Desktop\\VIKO 4 Semestar\\Information security\\Password Manager\\Password Manager\\bin\\Debug\\Users\\" + HomeUsername.Text + ".txt";
-            FileEncrypt(path, keyy);
-            e.Cancel = true;
-            
-        }*/
-        public static byte[] GenerateRandomSalt()
-        {
-            byte[] data = new byte[32];
-
-            using (RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider())
-            {
-                for (int i = 0; i < 10; i++)
-                {
-                    
-                    rng.GetBytes(data);
-                }
-            }
-
-            return data;
-        }
-      
-
-        public void FileEncrypt(string inputFile, string password)
-        {
        
-            byte[] salt = GenerateRandomSalt();
-
-           
-            FileStream fsCrypt = new FileStream(inputFile + ".aes", FileMode.Create);
-
-            
-            byte[] passwordBytes = System.Text.Encoding.UTF8.GetBytes(password);
-
-           
-            RijndaelManaged AES = new RijndaelManaged();
-            AES.KeySize = 256;
-            AES.BlockSize = 128;
-            AES.Padding = PaddingMode.PKCS7;
-
-           
-            var key = new Rfc2898DeriveBytes(passwordBytes, salt, 50000);
-            AES.Key = key.GetBytes(AES.KeySize / 8);
-            AES.IV = key.GetBytes(AES.BlockSize / 8);
-
-      
-            AES.Mode = CipherMode.CFB;
-
-          
-            fsCrypt.Write(salt, 0, salt.Length);
-
-            CryptoStream cs = new CryptoStream(fsCrypt, AES.CreateEncryptor(), CryptoStreamMode.Write);
-
-            FileStream fsIn = new FileStream(inputFile, FileMode.Open);
-
-            
-            byte[] buffer = new byte[1048576];
-            int read;
-
-            try
-            {
-                while ((read = fsIn.Read(buffer, 0, buffer.Length)) > 0)
-                {
-                    Application.DoEvents(); 
-                    cs.Write(buffer, 0, read);
-                }
-
-               
-                fsIn.Close();
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Error: " + ex.Message);
-            }
-            finally
-            {
-                cs.Close();
-                fsCrypt.Close();
-                File.Delete(inputFile);
-            }
-        }
     }
 }
